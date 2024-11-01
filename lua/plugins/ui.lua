7b48a9026ac19f182ac99b2a3611742bfdcbab2d
jbrannlund <johanbrannlund1@gmail.com>
2024-11-01 21:44:24 +0100

    Treesitter

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -6,11 +6,19 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim",         },
-        event = "BufWinEnter"  -    
+        event = "BufWinEnter" 
     },
-        "lukas-reineke/indent-blankline.nvim" ,
+    {    
+ 	"lukas-reineke/indent-blankline.nvim" ,
            main = "ibl",            ---@module "ibl"
            ---@type ibl.config
            opts = {}, 
-},
-}
+	},
+   {
+
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufRead", "BufNewFile" },  
+        run = ":TSUpdate"  
+    },
+}, 
+} 
