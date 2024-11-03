b5f001411ae9f24e9358ce420f05412936572532
johanbrannlund1@gmail.com <johanbrannlund1@gmail.com>
2024-11-03 12:34:42 +0100

    load all lua files for lua/config and lua/user-config

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
 require("config.lazy")
+
+local load_lua_files = require("functions.load_lua_files")
+load_lua_files("lua/config")
+
+-- Load user-specific configurations
+load_lua_files("lua/user-config")
